b51e939aacf759a5ed2adf8b7b6c0003bc5a28b3
MClarke9k9 <the9k9@yahoo.com>
2021-06-09 12:47:21 -0400

    Object desciption updates

--- src/utils/portfolioObjects.js
+++ src/utils/portfolioObjects.js
@@ -11,7 +11,7 @@ const portfolioObjects = [
 	{
 		img:"/assets/img/substrak.png",
 		title:"Subtrack",
-		description:"",
+		description:"This applications intention was to help people keep track of their subscriptions along with how much it was costing them per month to help them with their budgeting.",
 		urlDemo:"https://glacial-shore-36326.herokuapp.com/",
 		urlCode:"https://github.com/MClarke9k9/",
 		details:["Materialize","ReactJS","Nodejs","MVC","Express","MySQL","Sequelize","Heroku"],
@@ -19,7 +19,7 @@ const portfolioObjects = [
 	{
 		img:"/assets/img/PORTFO.jpg",
 		title:"My Portfolio!",
-		description:".",
+		description:"Creating the Portfolio Cards to display my projects by using an object based approach for the information to append to each card along with Reacts component wrapping to display in their own section.",
 		urlDemo:"http://marksweetportfolio.herokuapp.com/",
 		urlCode:"https://github.com/MClarke9k9/MCPortfolio",
 		details:["Bootstrap","MongoDB","ExpressJS","ReactJS","NodeJS","Heroku"],
@@ -27,7 +27,7 @@ const portfolioObjects = [
 	{
 		img:"/assets/img/burger.jpg",
 		title:"HavaBurger",
-		description:".",
+		description:"The app will store every burger in a database, whether devoured or not.",
 		urlDemo:"https://fast-retreat-55192.herokuapp.com/",
 		urlCode:"https://github.com/MClarke9k9/Hava-Burger",
 		details:["Materialize","Handlebars","Nodejs","MVC","Express","JQuery","ORM","Sequelize","Heroku"],
@@ -35,7 +35,7 @@ const portfolioObjects = [
 	{
 		img:"/assets/img/learn.png",
 		title:"Learn Javascript",
-		description:".",
+		description:"With this timed Javascript test you will be able to test your on javascript while being time to further hone your skills!",
 		urlDemo:"https://mclarke9k9.github.io/Learn-Javascript/",
 		urlCode:"https://github.com/MClarke9k9/Learn-Javascript",
 		details:["Bootstrap","CSS3","DOM Manipulation","Javascript","jQuery",],
@@ -43,7 +43,7 @@ const portfolioObjects = [
 	{
 		img:"/assets/img/ecom.png",
 		title:"EcomWebsite Mock-up",
-		description:".",
+		description:"This is a EcomWebsite Mock-up I created while trying to learn how to use Sliders and make good looking landing pages",
 		urlDemo:"https://mclarke9k9.github.io/EcomWebsite/",
 		urlCode:"https://github.com/MClarke9k9/EcomWebsite",
 		details:["Bootstrap","CSS3","Javascript","JQuery",],
@@ -51,7 +51,7 @@ const portfolioObjects = [
 	{
 		img:"/assets/img/wether.jpg",
 		title:"WeatherDashboard!",
-		description:".",
+		description:"You as traveler might want to see the weather outlook for multiple cities, so that I can plan a trips accordingly.",
 		urlDemo:"https://mclarke9k9.github.io/MyWeatherDashboard/",
 		urlCode:"https://github.com/MClarke9k9/MyWeatherDashboard",
 		details:["Bootstrap","CSS3","Javascript","AJAX","jQuery",],
@@ -59,7 +59,7 @@ const portfolioObjects = [
 	{
 		img:"/assets/img/planner.png",
 		title:"DayPlanner",
-		description:".",
+		description:"Introduction As an myself employee with a busy schedule I wanted to add important events to a daily planner so that I can manage my time effectively, and this is the app I created to do just that.",
 		urlDemo:"https://mclarke9k9.github.io/YourDayPlanner/",
 		urlCode:"https://github.com/MClarke9k9/YourDayPlanner",
 		details:["Bootstrap","CSS3","DOM Manipulation","Javascript","jQuery","Momentjs",],
@@ -67,7 +67,7 @@ const portfolioObjects = [
 	{
 		img:"/assets/img/fb.jpg",
 		title:"FacebookAudienceFinder",
-		description:".",
+		description:"This is a Facebook Marketing Keyword Finder app that gives you the audience number per word given to help cut marketing keyword cost.",
 		urlDemo:"https://modernmediamichael.github.io/firstproject/",
 		urlCode:"https://github.com/modernmediamichael/firstproject",
 		details:["Bootstrap","CSS3","DOM Manipulation","Javascript","jQuery",],
@@ -75,7 +75,7 @@ const portfolioObjects = [
 	{
 		img:"/assets/img/password.png",
 		title:"PasswordGenerator",
-		description:".",
+		description:"I understand companies deal with sensitive data day in and day out. I wanted create a randomly generated a password that can meet the specific criteria so that you can create a strong password that provides greater security for your company.",
 		urlDemo:"https://mclarke9k9.github.io/PasswordGenerator/",
 		urlCode:"https://github.com/MClarke9k9/PasswordGenerator",
 		details:["HTML","CSS3","Javascript",],
